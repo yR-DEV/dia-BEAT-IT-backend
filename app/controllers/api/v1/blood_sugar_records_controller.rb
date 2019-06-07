04b8c2d41c42eb6670ce07aecfb71aeaf369aa46
yeah-right <claar.zack@gmail.com>
2019-06-06 19:33:32 -0600

    Changed some of the records model to better sort and graph the data on the front end

--- app/controllers/api/v1/blood_sugar_records_controller.rb
+++ app/controllers/api/v1/blood_sugar_records_controller.rb
@@ -18,51 +18,7 @@ class Api::V1::BloodSugarRecordsController < ApplicationController
     private 
 
     def record_params 
-        params.permit(:user_id, :blood_sugar, :record_time, :record_date, :carbs, :insulin_units)
+        params.permit(:user_id, :blood_sugar, :time_of_day, :carbs, :insulin_units)
     end
 end
 
-
-  
-#     # GET /items/1
-#     def show
-#       render json: @item
-#     end
-  
-#     # POST /items
-#     def create
-#       @item = Item.new(item_params)
-  
-#       if @item.save
-#         render json: @item, status: :created, location: @item
-#       else
-#         render json: @item.errors, status: :unprocessable_entity
-#       end
-#     end
-  
-#     # PATCH/PUT /items/1
-#     def update
-#       if @item.update(item_params)
-#         render json: @item
-#       else
-#         render json: @item.errors, status: :unprocessable_entity
-#       end
-#     end
-  
-#     # DELETE /items/1
-#     def destroy
-#       @item.destroy
-#     end
-  
-#     private
-#       # Use callbacks to share common setup or constraints between actions.
-#       def set_item
-#         @item = Item.find(params[:id])
-#       end
-  
-#       # Only allow a trusted parameter "white list" through.
-#       def item_params
-#         params.require(:item).permit(:name, :description)
-#       end
-#   end
-  
